467e918ce04a10072c5fe5bf18e710e4c0d968c5
yuto-mochizuki <yuto-mochizuki@cybozu.co.jp>
2020-03-29 14:46:34 +0900

    Change app list design

--- src/js/component/template.js
+++ src/js/component/template.js
@@ -1,5 +1,7 @@
 const template = `
-<div class="kv-bar"></div>
+<div class="kv-bar">
+  <img src="https://biztel.jp/wp-content/uploads/2020/01/logo_kintone_mark_rgb.png">
+</div>
 <div class="kv-app-list"></div>
 <div class="kv-close">
   <i class="fas fa-times"></i>
--- src/js/logic/ListLogic.js
+++ src/js/logic/ListLogic.js
@@ -5,15 +5,29 @@ const ListLogic = {
 
   setAppList() {
     return App.getAppList('【Gr⇔kin連携】').then(response => {
+      $('.kv-app-list').html('');
+      
       const apps = response.apps;
       const $list = $('<div>');
+      
+      const $listTitle = $('<div class="kv-app-list-title">');
+      const $listTitleIcon = $('<img>').attr('src', 'https://static.cybozu.com/contents/k/image/argo/uiparts/widget/apps_56.png');
+      const $listTitleText = $('<span>').html('アプリ');
+      $listTitle.append($listTitleIcon);
+      $listTitle.append($listTitleText);
+      $('.kv-app-list').append($listTitle);
+
       apps.forEach(app => {
-        const $item = $('<div class="kv-app-item">').html(app.name);
+        const $appIcon = $('<img>').attr('src', 'https://static.cybozu.com/contents/k/image/icon/app/appTableBlue.png');
+        const $appName = $('<span>').html(app.name);
+        const $item = $('<div class="kv-app-item">');
+        $item.append($appIcon);
+        $item.append($appName);
+
         EventSetter.setListClickEvent($item, app.appId);
         $list.append($item);
       });
 
-      $('.kv-app-list').html('');
       $('.kv-app-list').append($list);
     });
   }
